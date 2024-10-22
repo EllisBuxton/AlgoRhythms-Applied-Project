489b8e979ae2ec90dd780b8502b8eb1fed561d36
EllisBuxton <g00406866@atu.ie>
2024-10-22 18:19:31 +0100

    Add index and instruments routes and HTML template rendering
    - Created an index route to serve the main page.
    - Created the instruments route  that Gets and Posts to a form.
    - The instruments route retrieves selected instruments from a form and returns them as JSON.

--- algorhythms_app/app.py
+++ algorhythms_app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+    return render_template('index.html')
+
+
+# Instruments route
+@app.route('/instruments', methods=['GET', 'POST'])
+def select_instruments():
+    if request.method == 'POST':
+        # Retreive Selected Instruments from the form
+        selected_instruments = request.form.getlist('instruments')
+        return jsonify({'instruments': selected_instruments})
+    return render_template('instruments.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
