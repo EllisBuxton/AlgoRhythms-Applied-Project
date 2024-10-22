ae176e3ff5503a2737062330da6951cb8473207c
EllisBuxton <g00406866@atu.ie>
2024-10-22 21:27:08 +0100

    Added Crossover and Mutation functions
    - Implemented a crossover function that combines parent melodyies to create a child melody
    
    -Implemented a mutation function that adds random note to child

--- algorhythms_app/genetic_algorithm.py
+++ algorhythms_app/genetic_algorithm.py
@@ -18,3 +18,16 @@ def selection(population, fitness_scores, num_to_select=5):
     # Select top num_to_select melodies based on fitness
     selected = sorted(zip(population, fitness_scores), key=lambda x: x[1], reverse=True)
     return [melody for melody, score in selected[:num_to_select]]
+
+# Crossover: Combine two parent melodies to create a child melody
+def crossover(parent1, parent2):
+    # Simple one-point crossover
+    crossover_point = len(parent1) // 2
+    return parent1[:crossover_point] + parent2[crossover_point:]
+
+# Mutation: Randomly mutate a melody
+def mutate(melody, mutation_rate=0.1):
+    for i in range(len(melody)):
+        if random.random() < mutation_rate:
+            melody[i] = random.randint(60, 72)  # Mutate the note
+    return melody
